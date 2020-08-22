d02b556b093fc7eb4413477cdd740b555556ce5d
Joaquim Dmt <joaquim.dimicode@users.noreply.github.com>
2020-08-22 18:31:39 +0200

    Landing page

---
+++ src/components/Login.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './styles/Login.css'
+
+function Login() {
+    return (
+        <div className="login">
+            <div className="logos">
+                <img src="/images/spotify-green.png" alt="logo de Spotify"/>
+                <span>X</span>
+                <img src="/images/beevora.png" alt="logo de BeeVora"/>
+           </div>
+           <a href="">LOGIN WITH SPOTIFY</a>
+        </div>
+    )
+}
+
+export default Login
